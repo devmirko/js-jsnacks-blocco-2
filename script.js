e4bef7a74fa16c0a7030b841b3a84c68e0f56799
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-11 16:49:22 +0200

    fine esercizio

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 //crea un array vuoto
 let myArray = [];
-let userNumber 
+let userNumber ;
 
 //ciclo: continua a chiedere i numeri all'utente
-i = 0
-total = 0
-while (  myArray.length < 10) {
+i = 0 
+total = 0 
+while ( total < 50) {
     userNumber = parseInt(prompt("inserisci un numero"));
     myArray.push(userNumber);
     total +=  myArray[i];
-   
-  
-    //condizione di uscita dal ciclo: fino a quando la somma degli elementi e minore di 50
-   
 }
 
 console.log(myArray);
